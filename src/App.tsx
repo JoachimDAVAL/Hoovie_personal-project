9c3a65a86d7a9f1d21caa558456304ba61ad2ea1
JoachimDAVAL <joachim.daval01@gmail.com>
2025-03-09 17:44:50 +0100

    display of flatproviders on MovieDetail

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,18 @@
 import './App.css'
 import { Routes, Route } from 'react-router';
-import { useState } from 'react';
-import { IMovie } from './@types';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import PopularMoviesList from './pages/PopularMoviesList';
-import MovieDetail from './pages/MovieDetail';
+import {MovieDetail} from './pages/MovieDetail';
 import MoviesByGenre from './pages/Movies ByGenre';
 import SearchPage from './pages/SearchPage';
 
 
-
-
-
 function App() {
-  const [query, setQuery] = useState("");
-  const [movies, setMovies] = useState<IMovie[]>([]);
-
-  const handleSearchResults = (newMovies: IMovie[]) => {
-    setMovies(newMovies);
-  };
+  
   return (
     <div className='App'>
-      <Header query={query} setQuery={setQuery} onResults={handleSearchResults} />
+      <Header />
       <main>
         <Routes>
           <Route path="/" element={<PopularMoviesList />} />
